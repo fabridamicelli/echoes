dab6dbe3c4101dbf7adfc834e4481a8a69d1f7d7
fabridamicelli <f.damicelli@uke.de>
2020-06-03 12:30:58 +0200

    make pip installable!

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
-from setuptools import setup
-from setuptools import find_packages
+from setuptools import setup, find_packages
 
 
-setup(name='echoes',
-      version='0.1',
-      description='Machine Learning with Echo State Networks',
-      author='Fabrizio Damicelli',
-      author_email='f.damicelli@uke.de',
-      url='',
-      license='',
-      install_requires=[],
-      classifiers=[
-          'Intended Audience :: Science/Research',
-          'Programming Language :: Python :: 3.7',
-      ],
-      packages=find_packages())
+setup(
+    name='echoes',
+    version='0.0.1',
+    description='Machine Learning with Echo State Networks',
+    author='Fabrizio Damicelli',
+    author_email='f.damicelli@uke.de',
+    url="https://github.com/fabridamicelli/echoes",
+    packages=find_packages(exclude=['notebooks', 'docs']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    python_requires='>=3.6',
+)
